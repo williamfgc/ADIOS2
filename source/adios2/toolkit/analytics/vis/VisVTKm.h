91117bba4e970e006315a94218b05f42bee1f161
Godoy, William F <godoywf@ornl.gov>
2017-08-16 13:01:53 -0400

    Creating fork

---
+++ source/adios2/toolkit/analytics/vis/VisVTKm.h
@@ -0,0 +1,26 @@
+/*
+ * VisVTKm.h
+ *
+ *  Created on: Aug 16, 2017
+ *      Author: William F Godoy
+ */
+
+#ifndef ADIOS2_TOOLKIT_ANALYTICS_VIS_VISVTKM_H_
+#define ADIOS2_TOOLKIT_ANALYTICS_VIS_VISVTKM_H_
+
+namespace adios2
+{
+
+class VisVTKm
+{
+public:
+    VisVTKm(const bool debugMode);
+
+    virtual ~VisVTKm();
+
+private:
+};
+
+} // end namespace adios2
+
+#endif /* SOURCE_ADIOS2_TOOLKIT_ANALYTICS_VIS_VISVTKM_H_ */
