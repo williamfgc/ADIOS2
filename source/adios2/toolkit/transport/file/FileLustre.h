f1b8eb7bb9f311ae195b3428bd7ebf100c2237b3
Wan <lipengwan86@gmail.com>
2017-11-20 10:17:58 -0500

    start lustre transport

---
+++ source/adios2/toolkit/transport/file/FileLustre.h
@@ -0,0 +1,9 @@
+/*
+ * Distributed under the OSI-approved Apache License, Version 2.0.  See
+ * accompanying file Copyright.txt for details.
+ *
+ * FileLustre.h wrapper of Lustre API functions for file I/O
+ *
+ *  Created on: Nov 20, 2017
+ *      Author: Lipeng Wan wanl@ornl.gov
+ */
